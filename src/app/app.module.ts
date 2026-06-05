940a016c1f8c8fbcc70dde5fc3f5de74fd69f3a4
Janier Leudo <janierleudo@gmail.com>
2026-06-05 11:51:24 -0500

    feat: creacion del codigo basico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './feature/products/products.component';
+import { ProductPageComponent } from './features/pages/product-page/product-page.component';
+import { ProductFormComponent } from './features/pages/components/product-form/product-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductPageComponent,
+    ProductFormComponent
   ],
   imports: [
     BrowserModule
